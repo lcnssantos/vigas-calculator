a2e679c6ffaccf020bc42e9c8c4a305da61ad5dc
Léo Freitas <leofreitas.engineer@gmail.com>
2022-05-10 12:01:29 -0300

    add chat sample

--- src/components/content/panel.tsx
+++ src/components/content/panel.tsx
@@ -2,6 +2,7 @@ import {Tabs, Tab} from "react-bootstrap";
 import React, {useState} from "react";
 import {Formulas} from "../../types/props";
 import FormulaBox from "./formulaBox";
+import Chart from "./chart";
 
 const Panel: React.FC<Formulas> = ({formula}) => {
     const [key, setKey] = useState('formula');
@@ -13,14 +14,15 @@ const Panel: React.FC<Formulas> = ({formula}) => {
             onSelect={(k) => setKey(k || 'formula')}
             className="mb-3"
         >
-            <Tab eventKey="formula" title="Fórmulas">
+            <Tab eventKey="formula" title="Somatório de Forças">
                 <FormulaBox formula={formula}/>
             </Tab>
 
             <Tab eventKey="internalForces" title="Esforços Internos" disabled>
             </Tab>
 
-            <Tab eventKey="diagram" title="Diagramas" disabled>
+            <Tab eventKey="diagram" title="Diagramas">
+                <Chart/>
             </Tab>
         </Tabs>
     );
